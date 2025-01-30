97a828dae323774e01000c07c72657c74e346a85
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2025-01-30 10:09:24 +0100

    increase the error rate

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const randomNumber = Math.floor(Math.random() * 1000)
-  if (randomNumber > 800) throw new Error(`The random number is ${randomNumber} I don't feel like returning a number greater than 500`)
+  if (randomNumber > 300) throw new Error(`The random number is ${randomNumber} I don't feel like returning a number greater than 500`)
   res.status(200).json({ randomNumber });
 }
